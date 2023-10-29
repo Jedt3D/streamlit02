5622d29deaf3825f838cc7fbc4854b74dffdfeac
Worajedt Sitthidumrong <sjedt@3ddaily.com>
2023-10-29 11:38:03 +0700

    add 3 charts

--- app.py
+++ app.py
@@ -8,21 +8,24 @@ st.title('Layout and Decoration')
 st.write("""
 เราจะลองทำ San Francisco Dataset กันดู
 """)
-col1, col2, col3 = st.columns(3)
-with col1:
-    st.write('Column 1')
-with col2:
-    st.write('Column 2')
-with col3:
-    st.write('Column 3')
 
 trees_df = pd.read_csv('trees.csv')
 df_dbh_grouped = pd.DataFrame(
     trees_df.groupby(['dbh']).count()['tree_id'])
 df_dbh_grouped.columns = ['tree_count']
-st.line_chart(df_dbh_grouped)
+
+col1, col2, col3 = st.columns(3)
+with col1:
+    st.line_chart(df_dbh_grouped)
+with col2:
+    st.bar_chart(df_dbh_grouped)
+with col3:
+    st.area_chart(df_dbh_grouped)
+
 st.caption('กราฟ แสดงจำนวนต้นไม้ จัดกลุ่มตามเส้นผ่าศูนย์กลาง')
 st.title('แปลผล')
 st.write("""
 ส่วนใหญ่ของต้นไม้ใน SF มีเส้นผ่าศูนย์กลาง 3' (2,721 ต้น)
 """)
+
+
